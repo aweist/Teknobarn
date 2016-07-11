3297bd51bf1ca9da1ebdc616fdd59c9ef20ab5f0
Andrew Weist <andrew.weist@gmail.com>
2016-07-11 13:02:25 -0400

    Add size to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -84,7 +84,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :rsvp)
+      params.require(:user).permit(:first_name, :last_name, :email, :rsvp, :size)
     end
 
 end
